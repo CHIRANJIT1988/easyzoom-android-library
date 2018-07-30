bcff2f36b4cf297a421e7207ae63fecd0a0964d9
CHIRANJIT BARDHAN <chiranjit.bardhan@conversionbug.com>
2018-07-30 11:27:19 +0530

    Initial Commit

--- app/src/main/java/com/easy/imageviewer/MainActivity.java
+++ app/src/main/java/com/easy/imageviewer/MainActivity.java
@@ -1,34 +1,14 @@
 package com.easy.imageviewer;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.easyzoom.EasyZoomImageViewer;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
 
     EasyZoomImageViewer image;
-
-    // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
-    public static final int CONNECTION_TIMEOUT = 10000;
-    public static final int READ_TIMEOUT = 15000;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,119 +19,5 @@ public class MainActivity extends AppCompatActivity {
         image = findViewById(R.id.imgFullScreen);
 
         image.with(R.drawable.demo);
-
-        //Make call to AsyncRetrieve
-        //new AsyncRetrieve().execute();
-
-        new OkHttpClient().execute();
-    }
-
-
-    private class AsyncRetrieve extends AsyncTask<String, String, String> {
-        ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
-        HttpURLConnection conn;
-        URL url = null;
-
-        //this method will interact with UI, here display loading message
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            pdLoading.setMessage("\tLoading...");
-            pdLoading.setCancelable(false);
-            pdLoading.show();
-
-        }
-
-        // This method does not interact with UI, You need to pass result to onPostExecute to display
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                // Enter URL address where your php file resides
-                url = new URL("https://api.androidhive.info/json/airline-tickets.php");
-
-            } catch (MalformedURLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return e.toString();
-            }
-            try {
-
-                // Setup HttpURLConnection class to send and receive data from php
-                conn = (HttpURLConnection) url.openConnection();
-                //conn.setReadTimeout(READ_TIMEOUT);
-                //conn.setConnectTimeout(CONNECTION_TIMEOUT);
-                conn.setRequestMethod("GET");
-
-                // setDoOutput to true as we recieve data from json file
-                //conn.setDoOutput(false);
-
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-                return e1.toString();
-            }
-
-            try {
-
-                int response_code = conn.getResponseCode();
-
-                // Check if successful connection made
-                if (response_code == HttpURLConnection.HTTP_OK) {
-
-                    // Read data sent from server
-                    InputStream input = conn.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder result = new StringBuilder();
-                    String line;
-
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-
-                    // Pass data to onPostExecute method
-                    return (result.toString());
-
-                } else {
-
-                    return ("unsuccessful");
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return e.toString();
-            } finally {
-                conn.disconnect();
-            }
-
-
-        }
-
-        // this method will interact with UI, display result sent from doInBackground method
-        @Override
-        protected void onPostExecute(String result)
-        {
-            //Log.i("onPostExecute", "" + result);
-
-            pdLoading.dismiss();
-
-            try
-            {
-                JSONArray array = new JSONArray(result);
-
-                for(int i=0; i<array.length(); i++)
-                {
-                    JSONObject json = array.getJSONObject(i);
-                    String string = json.getString("from");
-
-                    Log.i("onPostExecute", string);
-                }
-            }
-
-            catch (JSONException e)
-            {
-                e.printStackTrace();
-            }
-        }
     }
 }
